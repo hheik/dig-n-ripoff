d58534057392eed402ca77bfc4aaf9b24d68277e
hheik <4469778+hheik@users.noreply.github.com>
2022-08-26 15:57:39 +0300

    change: forgot to remove the funny camera in the last commit, so I made it better

--- src/systems/camera_control.rs
+++ src/systems/camera_control.rs
@@ -9,8 +9,16 @@ impl<'a> System<'a> for CameraControl {
     );
 
     fn run(&mut self, (time, mut camera): Self::SystemData) {
-        camera.transform.set_position(-Vector2F::ONE * 256.0);
+        camera.transform.set_position(-Vector2F::ONE * 64.0);
         let lifetime = time.lifetime.elapsed().unwrap().as_secs_f32();
-        camera.transform.set_scale(Vector2F::ONE * (lifetime.sin() * 0.5 + 0.5) * 4.0);
+        camera.transform.set_scale(Vector2F { 
+            x: (lifetime.sin() * 0.5 + 0.5) * 4.0, 
+            y: (lifetime.cos() * 0.5 + 0.5) * 4.0 
+        });
+
+        camera.transform.set_position(Vector2F { 
+            x: ((lifetime * 2.0).sin() * 0.5 + 0.5) * 128.0 - 128.0, 
+            y: ((lifetime * 2.0).cos() * 0.5 + 0.5) * 128.0 - 128.0 
+        });
     }
 }
